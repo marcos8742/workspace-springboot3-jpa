5ac62f44cd91641dc0b69a6da7ee9d94f13a9243
GESTOR SERVIÇOS - Marcos Brito Mendes <mendes.marcos@ebserh.gov.br>
2025-01-09 15:34:31 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.educandoweb.curso.entites.User;
+import com.educandoweb.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
